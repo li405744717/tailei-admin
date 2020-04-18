f02fc75c0ba59f724aea262e137dc599c158583b
dpwang <hippieDP@163.com>
2020-04-18 10:07:55 +0800

    init

--- src/components/headerBar/view.jsx
+++ src/components/headerBar/view.jsx
@@ -6,7 +6,17 @@ import Config from "@/common/config";
 export default function renderView(page) {
   var {} = page.state
   return (<div className="headerBar_container flex_row align_center">
-    tool bar
+    <div className='flex_row align_center header_bar_left_view'>
+      <img src={require('@/img/header_logo.png')} className='header_logo margin_left_80'/>
+      <span className='text_40 white mid margin_left_16'>安居公社</span>
+    </div>
+    <div className='flex_1 flex_row align_center padding_LR_48 header_bar_right_view'>
+      <img src={require('@/img/icon_menu.png')} className='icon_menu'/>
+      <div className='flex_1'/>
+      <img src={require('@/img/icon_bell.png')} className='icon_bell margin_right_40'/>
+      <img src={require('@/img/icon_user.png')} className='icon_user margin_right_32'/>
+      <span>用户名</span>
+    </div>
   </div>)
 }
 
